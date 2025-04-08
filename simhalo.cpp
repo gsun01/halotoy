c44319752b829e8989248b7e272145036c590a97
Guotong Sun <guotongsun01@gmail.com>
2025-04-08 14:19:15 -0500

    sample phi separately; input parameters with struct

--- simhalo.cpp
+++ simhalo.cpp
@@ -11,15 +11,10 @@ int main() {
     double z = 0.151;
     double B = 1.0e-15;
     // jet and viewing angles of GRB221009A: https://arxiv.org/pdf/2301.01798
-    double theta_jet = 1.5 * M_PI/180.0;
-    double theta_view = 2.0/3.0 * theta_jet;
-    // double theta_view = 0.0;
-
-    // theta_jet is the half opening angle
-    double theta_emi_min = theta_view - theta_jet;
-    double theta_emi_max = theta_view + theta_jet;
+    double th_j = 1.5 * M_PI/180.0;
+    double th_v = 2.0/3.0 * th_j;
     
-    std::ofstream file("photon_data.csv");
+    std::ofstream file("data.csv");
     file << "E,theta_obs,phi_obs,T\n";
     
     #pragma omp parallel
@@ -29,32 +24,41 @@ int main() {
         int thread_id = omp_get_thread_num();
         std::mt19937 rng(rd() + thread_id);
         std::uniform_real_distribution<double> energy_dist(2, 20);
-        std::uniform_real_distribution<double> thetap_dist(theta_emi_min, theta_emi_max);
+        std::uniform_real_distribution<double> theta_dist(-th_j, th_j);
         std::uniform_real_distribution<double> phi_dist(0, 2.0*M_PI);
         
         #pragma omp for schedule(static)
         for (int i = 0; i < 10000; ++i) {
             std::stringstream localBuffer; // thread-local buffer for photon data
-            
-            // if (i % 1000 == 0) {
-            //     #pragma omp critical
-            //     std::cout << "Simulating photon " << i << std::endl;
-            // }
-            
+
             double E = energy_dist(rng);
             
-            for (int j = 0; j < 10000000; ++j) {
-                double theta_p = thetap_dist(rng);
-                double phi_p = phi_dist(rng);
-                Photon photon(E, theta_p, phi_p, z, B);
-                if (photon.is_obs) {
-                    localBuffer << photon.E << ","
-                                << photon.theta_obs << ","
-                                << photon.phi_obs << ","
-                                << photon.T << "\n";
+            for (int j = 0; j < 100000; ++j) {
+                double th_emj = theta_dist(rng);
+                for (int k = 0; k < 1000; ++k) {
+                    // Generate a random azimuthal angle
+                    double phi_emj = phi_dist(rng);
+                    // Create a Photon object and propagate it
+                    GRB_params params;
+                    params.z = z;
+                    params.E = E;
+                    params.B0 = B;
+                    params.th_emj = th_emj;
+                    params.phi_emj = phi_emj;
+                    params.th_jet = th_j;
+                    params.th_view = th_v;
+                    params.mfp_seed = rd() + thread_id;
+                    Photon photon(params);
+                    photon.propagate_photon();
+                    // Check if the photon is observed
+                    if (photon.is_obs) {
+                        localBuffer << photon.E << ","
+                                    << photon.th_obs << ","
+                                    << photon.phi_obs << ","
+                                    << photon.T << "\n";
+                    }
                 }
             }
-
             #pragma omp critical
             {
                 file << localBuffer.str();
