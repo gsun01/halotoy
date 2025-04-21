7645bcaa62fe8ec48b40b098b4f3988192294dcf
Guotong Sun <guotongsun01@gmail.com>
2025-04-21 12:39:02 -0500

    avoid calculating certain parameters repeatedly

--- photon.h
+++ photon.h
@@ -9,15 +9,14 @@
 #include "helper.h"
 
 struct GRB_params {
-  double z; // redshift of GRB
-  double E; // energy of primary photon in TeV
-  double B0; // strength of IGMF in Gauss
+  double d_E; // comoving distance to GRB in Mpc
+  double d_gamma; // comoving distance traveled by photon before scattering in Mpc
+  double delta; // scattering angle in radians
+
   double th_emj; // emission angle w.r.t. the jet axis in radians
   double phi_emj; // emission angle w.r.t. the line of sight in radians
   double th_jet; // half opening angle of jet in radians
   double th_view; // viewing angle in radians
-
-  unsigned int mfp_seed;
 };
 
 class Photon {
@@ -70,32 +69,32 @@ private:
     calc_th_emi();
   }
 
-  void calc_distances() {
-    // d_E: comoving distance to GRB
-    auto f1 = [=](double z) {
-      return 3.0e5/(70*std::sqrt(0.3*(1+z)*(1+z)*(1+z)+0.7));
-    };
-    d_E = adaptiveGaussQuadrature(f1, 0, z);
+  // void calc_distances() {
+  //   // d_E: comoving distance to GRB
+  //   auto f1 = [=](double z) {
+  //     return 3.0e5/(70*std::sqrt(0.3*(1+z)*(1+z)*(1+z)+0.7));
+  //   };
+  //   d_E = adaptiveGaussQuadrature(f1, 0, z);
 
-    // z_s: redshift of scattering event
-    z_s = calc_z(E, z, 0.0, z);
+  //   // z_s: redshift of scattering event
+  //   z_s = calc_z(E, z, 0.0, z);
 
-    // mfp: comoving mean free path of photon before scattering
-    auto f2 = [=](double z) {
-      return -3.0e5/(70*std::sqrt(0.3*(1+z)*(1+z)*(1+z)+0.7));
-    };
-    mfp = adaptiveGaussQuadrature(f2, z, z_s);
+  //   // mfp: comoving mean free path of photon before scattering
+  //   auto f2 = [=](double z) {
+  //     return -3.0e5/(70*std::sqrt(0.3*(1+z)*(1+z)*(1+z)+0.7));
+  //   };
+  //   mfp = adaptiveGaussQuadrature(f2, z, z_s);
 
-    // d_gamma: comoving distance traveled by photon before scattering
-    std::mt19937 rng(mfp_seed);
-    std::exponential_distribution<double> exp_dist(1.0/mfp);
-    d_gamma = exp_dist(rng);
-  }
+  //   // d_gamma: comoving distance traveled by photon before scattering
+  //   std::mt19937 rng(mfp_seed);
+  //   std::exponential_distribution<double> exp_dist(1.0/mfp);
+  //   d_gamma = exp_dist(rng);
+  // }
 
-  // scattering angle in radians
-  void calc_delta() {
-    delta = 3.0e-6 /(1+z_s)/(1+z_s) * (B0/1.0e-18) /(0.5*E/10)/(0.5*E/10);
-  }
+  // // scattering angle in radians
+  // void calc_delta() {
+  //   delta = 3.0e-6 /(1+z_s)/(1+z_s) * (B0/1.0e-18) /(0.5*E/10)/(0.5*E/10);
+  // }
 
   void calc_obs_angles() {
     th_obs = delta - th_emi;
@@ -123,22 +122,22 @@ private:
 
 public:
   Photon(GRB_params &params)
-    : E(params.E),
+    : d_E(params.d_E),
+      d_gamma(params.d_gamma),
+      delta(params.delta),
+
       th_emj(params.th_emj),
       phi_emj(params.phi_emj),
-      z(params.z),
-      B0(params.B0),
       th_jet(params.th_jet),
       th_view(params.th_view),
-      is_obs(false),
-      mfp_seed(params.mfp_seed) {};
+      is_obs(false) {};
 
   ~Photon() {};
 
   void propagate_photon() {    
     calc_emitting_angles();
-    calc_distances();
-    calc_delta();
+    // calc_distances();
+    // calc_delta();
     calc_obs_angles();
     is_observed();
     if (is_obs) {
@@ -149,18 +148,11 @@ public:
   double th_emj, phi_emj; // emission angles w.r.t. the jet axis in radians
   double th_emi, phi_emi; // emission angles w.r.t. the line of sight in radians
   double th_jet, th_view; // half opening angle of jet and viewing angle in radians
-  double E, th_obs, phi_obs; // energy of primary photon in TeV and the arrival direction 
-                             // at the observer in radians (screen)
+  double th_obs, phi_obs; // the arrival direction at the observer in radians (screen)
 
-  double z; // redshift of GRB
-  double z_s; // redshift of scattering event
-  double mfp; // comoving mean free path of photon
   double d_gamma; // comoving distance traveled by photon before scattering
   double delta; // scattering angle in radians
   double d_E; // comoving distance to GRB
   double T; // time delay due to scattering
   bool is_obs; // is the photon observed?
-
-  double B0; // strength of IGMF in G (free param)
-  unsigned int mfp_seed;
 };
